966fca49ec627206023a84716d7a7b7dafb5b0f0
utareyu <uta5315@gmail.com>
2023-06-19 15:12:59 +0900

    modified:   pages/index.js  new file:   public/router.png

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [Input, setInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ request: Input }),
       });
 
       const data = await response.json();
@@ -23,33 +23,33 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
-      alert(error.message);
+      alert(error.message);t
     }
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>OpenAI app</title>
+        <link rel="icon" href="/router.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Generate text to Cisco IOS Command</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="input"
+            placeholder="what you want to do?"
+            value={Input}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Command" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
